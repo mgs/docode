ccaee6fe4e2f43e7607aec6e5f3a3b3d9f794c68
mgs <mgsimpson@gmail.com>
2017-04-07 04:49:58 -0400

    made a function: say(msg,[array]) for debug output

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ var argv = require('yargs')
     .usage('Usage: $0 --screenshots=<sketchFolder> [options]')
     .example('$0 --screenshots=<sketchFolder>', 'Create documentation assets for the p5 sketch in <sketchFolder>')
     .alias('s', 'screenshots')
-    // .nargs('s')
+// .nargs('s')
     .describe('s', 'Specifies the sketch folder to compile into docs.')
     .alias('g', 'gif')
-    // .nargs('g')
+// .nargs('g')
     .describe('g', 'Create an animated gif of the specified sketch.')
     .alias('v', 'video')
-    // .nargs('v')
+// .nargs('v')
     .describe('v', 'Output an mp4 video of the specified sketch.')
     .alias('p', 'preview')
-    // .nargs('p')
+// .nargs('p')
     .describe('p', 'Shows an ascii preview of the sketch in the terminal.')
-    // .demandOption(['s'])
+// .demandOption(['s'])
     .help('h')
     .alias('h', 'help')
     .argv;
@@ -30,138 +30,122 @@ var currentFolder = process.cwd();
 var fileNameArray = [];
 
 var operations = {
-  screenshots: createScreenshots,
-  s: createScreenshots,
-  video: createVideo,
-  v: createVideo,
-  gif: createGif,
-  g: createGif,
-  preview: showPreview,
-  p: showPreview,
-  help: showHelp,
-  h: showHelp
+    screenshots: createScreenshots,
+    s: createScreenshots,
+    video: createVideo,
+    v: createVideo,
+    gif: createGif,
+    g: createGif,
+    preview: showPreview,
+    p: showPreview,
+    help: showHelp,
+    h: showHelp
 };
 
 function main (args){
-
-  for (var n in args){
-    if (operations.hasOwnProperty(n)){
-      if (args[n] !== undefined && args[n] !== false) {
-        operations[n]();
-        break;
-      }
+    for (var n in args){
+        if (operations.hasOwnProperty(n)){
+            if (args[n] !== undefined && args[n] !== false) {
+                operations[n]();
+                break;
+            }
+        }
     }
-  }
 }
 
-function reportErrors(args){
-
-  var syntaxError = false;
-  var mistakes = [];
-
-  if (args._.length > 0){
-    syntaxError = true;
-    for (var i=0; i<args._.length; i++){
-      mistakes.push(args._[i]);
-    }
-  }
-
-  if (syntaxError === true){
+function say(message, additionals){
+    console.log(" ");
     console.log("-------------------------------------------------------------------");
-    console.log("|" + clc.red(' ☝️  doCode Errors                                               ') + " |");
+    console.log("|" + message + " |");
     console.log("-------------------------------------------------------------------");
     console.log("|                                                                 |");
     console.log("|   The following arguments do not match doCode's command list:   |");
     console.log("|                                                                 |");
-    for (var n=0; n<mistakes.length; n++){
-      console.log("|    • " + mistakes[n] + (" ".repeat(59-mistakes[n].length)) + "|");
+    if(additionals){
+        for (var n=0; n<additionals.length; n++){
+            console.log("|    • " + additionals[n] + (" ".repeat(59-additionals[n].length)) + "|");
+        }
     }
     console.log("|                                                                 |");
     console.log("-------------------------------------------------------------------");
+}
 
-  }
+function reportErrors(args){
+    var syntaxError = false;
+    var mistakes = [];
+
+    if (args._.length > 0){
+        syntaxError = true;
+        for (var i=0; i<args._.length; i++){
+            mistakes.push(args._[i]);
+        }
+    }
+
+    if (syntaxError === true){
+        var msg = clc.red(" ☝️' doCode Errors");
+        say(msg, mistakes);
+    }
 }
 
 function createScreenshots(operations){
-  var source, target;
+    var source, target;
 
-  if (typeof(argv.s) === 'boolean'){
-    target = currentFolder + '/docode_screenshots/sketch.png';
-    source = 'file:///' + currentFolder + '/index.html';
-  } else {
-    argv.s = argv.s.replace('~', os.homedir());
-    target = argv.s + '/docode_screenshots/sketch.png';
-    source = 'file:///' + argv.s + '/index.html';
-  }
-
-  renderWebpage(source, target, function (err) {
-    if (err){
-      throw err;
+    if (typeof(argv.s) === 'boolean'){
+        target = currentFolder + '/docode_screenshots/sketch.png';
+        source = 'file:///' + currentFolder + '/index.html';
+    } else {
+        argv.s = argv.s.replace('~', os.homedir());
+        target = argv.s + '/docode_screenshots/sketch.png';
+        source = 'file:///' + argv.s + '/index.html';
     }
 
-    // console.log('Success!');
-    var msg = " 🖼  👍  Screenshots were created successfully";
-    console.log(" ");
-    console.log("-------------------------------------------------------------------");
-    console.log("|" + clc.cyanBright(msg) + (" ".repeat(66-msg.length)) + " |");
-    console.log("-------------------------------------------------------------------");
-    console.log(" ");
-  });
+    renderWebpage(source, target, function (err) {
+        if (err){
+            throw err;
+        }
+
+        var msg = " 🖼  👍  Screenshots were created successfully";
+        say("|" + clc.cyanBright(msg) + (" ".repeat(66-msg.length)) + " |");
+    });
 }
 
 
 function createVideo(operations){
-  var msg = " 📹 😔  Video creation is not supported yet";
-  console.log(" ");
-  console.log("-------------------------------------------------------------------");
-  console.log("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
-  console.log("-------------------------------------------------------------------");
-  console.log(" ");
+    var msg = " 📹 😔  Video creation is not supported yet";
+    say("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
 }
 
 function createGif(operations){
-  var msg = " 🌅 😔  GIF creation is not supported yet";
-  console.log(" ");
-  console.log("-------------------------------------------------------------------");
-  console.log("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
-  console.log("-------------------------------------------------------------------");
-  console.log(" ");
+    var msg = " 🌅 😔  GIF creation is not supported yet";
+    say("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
 }
 
 function showPreview(operations){
-  var msg = " ☠️ 😔  Preview is not supported yet";
-  console.log(" ");
-  console.log("-------------------------------------------------------------------");
-  console.log("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
-  console.log("-------------------------------------------------------------------");
-  console.log(" ");
+    var msg = " 💡 😔  Preview is not supported yet";
+    say("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
 }
 
 function showHelp(operations){
-  var msg = " 💡 😔  Preview is not supported yet";
-  console.log("                                                                   ");
-  console.log("-------------------------------------------------------------------");
-  console.log("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
-  console.log("-------------------------------------------------------------------");
-  console.log("                                                                   ");
+    var msg = " 💡 😔  Preview is not supported yet";
+    say("|" + clc.cyan(msg) + (" ".repeat(66-msg.length)) + " |");
 }
 
 function renderWebpage (source, target, cb) {
-  var args = [renderer, source, target];
-  var child = spawn(phantomjs, args, { stdio: 'ignore' });
+    var args = [renderer, source, target];
+    var child = spawn(phantomjs, args, { stdio: 'ignore' });
 
-  // Very annoying but the precision on file modification time seems to preclude that from being used to sort our file order
-  // this is not a smart/clever way to workaround but .. it works.
-  for(var i = 0; i < 30; i++){
-    fileNameArray.push("sketch" + i + ".png");
-  }
+    // Very annoying but the precision on file modification time seems to preclude that from being used to sort our file order
+    // this is not a smart/clever way to workaround but .. it works.
+    for(var i = 0; i < 30; i++){
+        fileNameArray.push("sketch" + i + ".png");
+    }
 
-  child.on('error', cb);
+    child.on('error', cb);
     child.on('exit', function (code) {
-      if (code !== 0) {
-        return cb(new Error('Bad exit code: ' + code));
-      }
-      cb(null);
+        if (code !== 0) {
+            return cb(new Error('Bad exit code: ' + code));
+        }
+        cb(null);
     });
 }
 
